24c67077e1f89beca10dd372f1bb8355ecccf3f4
divyadhimaan <dhimandivya713@gmail.com>
2025-07-17 13:54:31 +0530

    blogs formatted

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -55,9 +55,9 @@ export default async function Blog({
 
   return (
     <Row fillWidth>
-      <Row maxWidth={12} hide="m"/>
+      {/* <Row maxWidth={12} hide="m"/> */}
       <Row fillWidth horizontal="center">
-        <Column as="section" maxWidth="xs" gap="l">
+        <Column as="section" maxWidth="s" gap="l">
           <Schema
             as="blogPosting"
             baseURL={baseURL}
@@ -89,7 +89,7 @@ export default async function Blog({
           <ScrollToHash />
         </Column>
     </Row>
-    <Column maxWidth={12} paddingLeft="40" fitHeight position="sticky" top="80" gap="16" hide="m">
+    {/* <Column maxWidth={12} paddingLeft="40" fitHeight position="sticky" top="80" gap="16" hide="m">
       <Row
         gap="12"
         paddingLeft="2"
@@ -101,7 +101,7 @@ export default async function Blog({
         On this page
       </Row>
       <HeadingNav fitHeight/>
-    </Column>
+    </Column> */}
     </Row>
   );
 }
